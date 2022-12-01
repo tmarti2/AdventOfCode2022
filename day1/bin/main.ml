f11b8cc35ca36957ac0b9e280d019555bfdb8d3b
Thibault Martin <twenty2bar@gmail.com>
2022-12-01 19:50:22 +0100

    Day 1 without Base

--- day1/bin/main.ml
+++ day1/bin/main.ml
@@ -1,26 +1,26 @@
-open Base
-
 let parse_file data =
-  let rec parse_elfs todo acc elfs =
-    match todo with
-    | [] -> acc::elfs
-    | "" :: tl -> parse_elfs tl 0 (acc::elfs)
-    | hd :: tl -> parse_elfs tl ((Int.of_string hd) + acc) elfs
+  let rec aux acc elfs =
+    try
+      match input_line data |> int_of_string_opt with
+      | None -> aux 0 (acc::elfs)
+      | Some x -> aux (acc+x) elfs
+    with
+      End_of_file -> acc::elfs
   in
-  parse_elfs data 0 []
+  aux 0 []
 
-let solve1 data = List.max_elt ~compare data |> Option.value ~default:0
+let solve1 data = List.fold_left max 0 data
 
 let solve2 data =
-  match List.sort ~compare data |> List.rev with
+  match List.sort compare data |> List.rev with
   | [] -> 0
   | elf1 :: [] -> elf1
   | elf1 :: elf2 :: [] -> elf1 + elf2
   | elf1 :: elf2 :: elf3 :: _ -> elf1 + elf2 + elf3
 
 let main file =
-  let data = Stdio.In_channel.read_lines file |> parse_file in
-  Stdio.printf "File : %s\n\tPart 1 : %d\n\tPart 2 : %d\n"
+  let data = open_in file |> parse_file in
+  Printf.printf "File : %s\n\tPart 1 : %d\n\tPart 2 : %d\n"
     file (solve1 data) (solve2 data)
 
 let _ = main "example.txt";  main "input.txt"
