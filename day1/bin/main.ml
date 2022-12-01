cc22cf4485c9548e94ff18cf70f07d483d30cb3f
Thibault Martin <twenty2bar@gmail.com>
2022-12-01 19:27:33 +0100

    Day 1 bis

--- day1/bin/main.ml
+++ day1/bin/main.ml
@@ -4,22 +4,15 @@ let parse_file data =
   let rec parse_elfs todo acc elfs =
     match todo with
     | [] -> acc::elfs
-    | "" :: tl -> parse_elfs tl [] (acc::elfs)
-    | hd :: tl -> parse_elfs tl ((Int.of_string hd)::acc) elfs
+    | "" :: tl -> parse_elfs tl 0 (acc::elfs)
+    | hd :: tl -> parse_elfs tl ((Int.of_string hd) + acc) elfs
   in
-  parse_elfs data [] []
+  parse_elfs data 0 []
 
-let solve1 data =
-  List.fold_left data ~init:0 ~f:(fun acc el ->
-      let max = List.sum (module Int) el ~f:(fun i -> i) in
-      if max > acc then max else acc
-    )
+let solve1 data = List.max_elt ~compare data |> Option.value ~default:0
 
 let solve2 data =
-  let all = List.fold_left data ~init:[]
-      ~f:(fun acc el -> (List.sum (module Int) el ~f:(fun i -> i)) :: acc
-    ) |> List.sort ~compare |> List.rev in
-  match all with
+  match List.sort ~compare data |> List.rev with
   | [] -> 0
   | elf1 :: [] -> elf1
   | elf1 :: elf2 :: [] -> elf1 + elf2
