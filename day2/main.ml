3dee5324142d12175aa6eaabde2ed1f077652642
Thibault Martin <twenty2bar@gmail.com>
2022-12-02 12:31:52 +0100

    [Day 2] cleaning

--- day2/main.ml
+++ day2/main.ml
@@ -30,25 +30,15 @@ end
 module Solving = struct
   open Base
 
-  let value = function
-    | R -> 1
-    | P -> 2
-    | S -> 3
+  let value = function R -> 1 | P -> 2 | S -> 3
 
-  let win = function
-    | R -> P
-    | P -> S
-    | S -> R
+  let win   = function R -> P | P -> S | S -> R
 
-  let lose = function
-    | R -> S
-    | P -> R
-    | S -> P
+  let lose  = function R -> S | P -> R | S -> P
 
-  let score = function
-    | (p1,p2) ->
-      (value p2) +
-      if Poly.(p1 = p2) then 3 else if Poly.(p2 = win p1) then 6 else 0
+  let score (p1,p2) =
+    (value p2) +
+    if Poly.(p1 = p2) then 3 else if Poly.(p2 = win p1) then 6 else 0
 
   let choose = function
     | (p1,R) -> (p1,lose p1)
@@ -56,12 +46,10 @@ module Solving = struct
     | (p1,S) -> (p1,win p1)
 
   let part1 (input : input) : output =
-    List.sum ~f:score (module Int) input
-    |> Option.some
+    List.sum ~f:score (module Int) input |> Option.some
 
   let part2 (input : input) : output =
-    List.sum ~f:(fun i -> score (choose i)) (module Int) input
-    |> Option.some
+    List.sum ~f:(Fn.compose score choose) (module Int) input |> Option.some
 end
 
 module Today = MakeDay(Types)(Parsing)(Solving)
