5a9b88a9869cbdf0835355c974f37b1156c10abf
Thibault Martin <twenty2bar@gmail.com>
2022-12-03 13:43:39 +0100

    [Day 3] Cleaning

--- day3/main.ml
+++ day3/main.ml
@@ -23,7 +23,7 @@ module Solving = struct
     let code = Char.to_int c - 64 in
     if code <= 26 then code + 26 else code - 32
 
-  let find_common1 s =
+  let find_common_part1 s =
     let len = String.length s in
     let comp1, comp2 =
       String.sub s ~pos:0 ~len:(len/2), String.sub s ~pos:(len/2) ~len:(len/2)
@@ -31,7 +31,7 @@ module Solving = struct
     String.find comp1 ~f:(fun c1 -> String.exists comp2 ~f:(Char.equal c1))
     |> Option.value_exn
 
-  let find_common2 s1 s2 s3 =
+  let find_common_part2 s1 s2 s3 =
     String.filter s1 ~f:(fun c1 -> String.exists s2 ~f:(Char.equal c1))
     |> String.find ~f:(fun c1 -> String.exists s3 ~f:(Char.equal c1))
     |> Option.value_exn
@@ -40,12 +40,12 @@ module Solving = struct
     match todo with
     | [] -> acc
     | e1 :: e2 :: e3 :: tl ->
-      let value = find_common2 e1 e2 e3 |> score in
-      aux_part2 tl (acc + value)
+      find_common_part2 e1 e2 e3 |> score |> (+) acc |> aux_part2 tl
     | _ -> assert false
 
   let part1 (input : input) : output =
-    List.sum ~f:(Fn.compose score find_common1) (module Int) input |> Option.some
+    List.sum ~f:(Fn.compose score find_common_part1) (module Int) input
+    |> Option.some
 
   let part2 (input : input) : output =
     aux_part2 input 0 |> Option.some
